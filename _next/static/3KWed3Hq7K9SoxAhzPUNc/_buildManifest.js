13cd3c5a7bcec3a965448fcb684ff7f5b50787ac
tketigr <ketigr@darasa.io>
2026-01-15 02:34:51 +0400

    Updates

--- _next/static/3KWed3Hq7K9SoxAhzPUNc/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-a647cd2c75dc4dc7.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
